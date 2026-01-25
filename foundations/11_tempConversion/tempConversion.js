f0d867db439b1252365d071f381237bc38d5afe3
josh.i.. <35184227+himanshuhjoshi@users.noreply.github.com>
2026-01-25 21:34:18 +0530

    JS exercises solved till foundation->11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const tempCelsius = (temp - 32) * (5/9);
+  return Number.isInteger(tempCelsius) ? tempCelsius : Number(tempCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const tempFahrenheit = (temp * (9/5) + 32);
+  return Number.isInteger(tempFahrenheit) ? tempFahrenheit : Number(tempFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
