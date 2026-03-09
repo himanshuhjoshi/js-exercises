31345530d32b1a39d970b79b8610127a3092d6e0
josh.i.. <35184227+himanshuhjoshi@users.noreply.github.com>
2026-03-09 09:05:12 +0530

    Completed exercises till 15

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, curr) => acc += curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => acc *= curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1; 
+  for(let i=num; i>1; i--){
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanedString = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"").toLowerCase();
+    const reverseredString = cleanedString.split('').reverse().join('').toLowerCase();
+    return cleanedString === reverseredString;
 };
 
 // Do not edit below this line
