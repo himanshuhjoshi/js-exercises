4724950d0e019a273a666cfa3d102c60f3a21107
josh.i.. <35184227+himanshuhjoshi@users.noreply.github.com>
2026-03-13 09:52:31 +0530

    completed JS exercises

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
 const findTheOldest = function(people) {
-   
+   return people.reduce(
+    (acc, curr) => ((acc.yearOfDeath || new Date().getFullYear()) - acc.yearOfBirth < (curr.yearOfDeath || new Date().getFullYear()) - curr.yearOfBirth) 
+    ? curr : acc)
 };
 
 // Do not edit below this line
